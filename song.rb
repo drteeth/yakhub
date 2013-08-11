678ed046ed609f2add6f5485503bef223f0f1cb1
davemaurakis <davemaurakis@gmail.com>
2013-08-11 11:49:47 -0400

    merged

--- song.rb
+++ song.rb
@@ -1,11 +1,13 @@
 class Song
 
   attr_accessor :key
+  attr_reader :background
 
   def initialize(key, file=MidiFile.new)
     @key = key
     @file = file
     @sections = []
+    @background
   end
 
   def add_section(section)
@@ -26,27 +28,28 @@ class Song
   def to_bytes
     @file.toBytes
   end
+
   def background
-    # track.setInstrument(2, 46)
-    track_one = Track.new
-    track_three = Track.new
-    track_five = Track.new
-    track_one.setInstrument(1, 95)
-    track_three.setInstrument(1, 95)
-    track_five.setInstrument(1, 95)
-    file.addTrack(track)
-    file.addTrack(track_one)
-    file.addTrack(track_three)
-    file.addTrack(track_five)
-    root = "#{scale.note_by_degree(1)}2"
-    third = "#{scale.note_by_degree(3)}3"
-    fifth = "#{scale.note_by_degree(5)}2"
-    # full_duration = commit_group.count * duration
+    # # track.setInstrument(2, 46)
+    # track_one = Track.new
+    # track_three = Track.new
+    # track_five = Track.new
+    # track_one.setInstrument(1, 95)
+    # track_three.setInstrument(1, 95)
+    # track_five.setInstrument(1, 95)
+    # file.addTrack(track)
+    # file.addTrack(track_one)
+    # file.addTrack(track_three)
+    # file.addTrack(track_five)
+    # root = "#{scale.note_by_degree(1)}2"
+    # third = "#{scale.note_by_degree(3)}3"
+    # fifth = "#{scale.note_by_degree(5)}2"
+    # # full_duration = commit_group.count * duration
 
-    track_one.addNote(1, root, full_duration)
-    track_three.addNote(1, third, full_duration)
-    track_five.addNote(1, fifth, full_duration)
+    # track_one.addNote(1, root, full_duration)
+    # track_three.addNote(1, third, full_duration)
+    # track_five.addNote(1, fifth, full_duration)
 
-# duration = Author::Duration
+    # duration = Author::Duration
   end
 end
